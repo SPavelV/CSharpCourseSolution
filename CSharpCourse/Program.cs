49d77de2577e0d609f8582c16bbb2bcb5ceea0e9
Pavel Solovev <pashius2010@gmail.com>
2020-12-20 23:03:27 +0400

    variables scoup

--- CSharpCourse/Program.cs
+++ CSharpCourse/Program.cs
@@ -7,7 +7,29 @@ namespace CSharpCourse
     {
         static void Main(string[] args)
         {
+           
+        }
 
+        static void VariablesScope()
+        {
+            var a = 1;
+            {
+                var b = 2;
+                {
+                    var c = 3;
+
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                }
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                // Console.WriteLine(c);
+            }
+
+            Console.WriteLine(a);
+            // Console.WriteLine(b);
+            // Console.WriteLine(c);
         }
 
         static void Literals()
