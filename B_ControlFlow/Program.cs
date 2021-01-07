a52172ffbaffc0a7b37e4c7011ace0141b49cb70
Pavel Solovev <pashius2010@gmail.com>
2021-01-07 15:08:56 +0400

    BreakContinue

--- B_ControlFlow/Program.cs
+++ B_ControlFlow/Program.cs
@@ -7,6 +7,71 @@ namespace B_ControlFlow
         static void Main(string[] args)
         {
             
+
+        }
+
+        static void BreakContinue()
+        {
+            int[] numbers = { 0, 3, 1, 5, 4, 6, 7, 8, 9 };
+
+            foreach (int n in numbers)
+            {
+                //if(n%2 == 0)
+                //{
+                //    Console.WriteLine(n);
+                //}
+
+                if (n % 2 != 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(n);
+            }
+
+            Console.ReadLine();
+
+            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Number={numbers[i]}");
+
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if (numbers[i] == j) break;
+                    Console.Write($" {letters[j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+            int[] numbers2 = { 1, -2, 4, -7, 5, 3, 2, -1, -3, 2, 7, -1, -3, 1, 7 };
+
+            int counter = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (counter == 3)
+                {
+                    break;
+                }
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int atI = numbers2[i];
+                    int atJ = numbers2[j];
+
+                    if (atI + atJ == 0)
+                    {
+                        Console.WriteLine($"Pair({atI};{atJ}). Indexes ({i};{j})");
+                        counter++;
+                    }
+
+                    if (counter == 3)
+                    {
+                        break;
+                    }
+                }
+            }
         }
 
         static void WhileDoWhile()
