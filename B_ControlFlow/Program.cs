cc775f295366788797fd7d1eaf775ee6f2ea7d58
Pavel Solovev <pashius2010@gmail.com>
2021-01-07 20:47:08 +0400

    switch, case

--- B_ControlFlow/Program.cs
+++ B_ControlFlow/Program.cs
@@ -6,8 +6,74 @@ namespace B_ControlFlow
     {
         static void Main(string[] args)
         {
-            
 
+          
+        }
+
+        static void SwithcCase()
+        {
+            int month = int.Parse(Console.ReadLine());
+            string season = string.Empty;
+
+            switch (month)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    season = "Winter";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    season = "Spring";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "Summer";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "Autumn";
+                    break;
+                default:
+                    throw new ArgumentException("Unexpected number of mounth");
+            }
+
+            Console.WriteLine(season);
+
+            Console.ReadLine();
+
+            int weddingYears = int.Parse(Console.ReadLine());
+
+            string name = string.Empty;
+
+            switch (weddingYears)
+            {
+                case 5:
+                    name = "Деревянная свадьба";
+                    break;
+                case 10:
+                    name = "Оловянная свадьба";
+                    break;
+                case 15:
+                    name = "Хрустальная свадьба";
+                    break;
+                case 20:
+                    name = "Форфоровая свадьба";
+                    break;
+                case 25:
+                    name = "Серебряная свадьба";
+                    break;
+                case 30:
+                    name = "Жемчужная свадьба";
+                    break;
+                default:
+                    name = "Не знаем такого юбелея";
+                    break;
+            }
+            Console.WriteLine(name);
         }
 
         static void BreakContinue()
