9a92045413f4efddf45aa06d5ee1de2baa4e7c5d
Pavel Solovev <pashius2010@gmail.com>
2021-01-07 12:34:27 +0400

    for, foreach

--- B_ControlFlow/Program.cs
+++ B_ControlFlow/Program.cs
@@ -6,7 +6,42 @@ namespace B_ControlFlow
     {
         static void Main(string[] args)
         {
-            GetMax();
+          
+        }
+
+        static void ForForeach()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                if (numbers[j] % 2 == 0)
+                {
+                    Console.Write(numbers[j] + " ");
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (int val in numbers)
+            {
+                Console.Write(val + " ");
+            }
+
         }
 
         static void GetMax()
