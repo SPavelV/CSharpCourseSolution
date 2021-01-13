37d6dff9a69734e5f45b77bf29111c292c60db23
Pavel Solovev <pashius2010@gmail.com>
2021-01-13 11:27:38 +0400

    type Array

---
+++ C_ArraysCollections/Program.cs
@@ -0,0 +1,56 @@
+﻿                using System;
+
+namespace C_ArraysCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void ArrayType()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int index = Array.BinarySearch(numbers, 7);
+            Console.WriteLine(index);
+
+            int[] copy = new int[10];
+            Array.Copy(numbers, copy, numbers.Length);
+
+            int[] anotherCopy = new int[10];
+            copy.CopyTo(anotherCopy, 0);
+
+            Array.Reverse(copy);
+            foreach (var c in copy)
+            {
+                Console.WriteLine(c);
+            }
+
+            Array.Sort(copy);
+            foreach (var c in copy)
+            {
+                Console.WriteLine(c);
+            }
+
+            Array.Clear(copy, 0, copy.Length);
+
+
+            int[] a1;
+            a1 = new int[5];
+
+            int[] a2 = new int[5];
+
+            int[] a3 = new int[5] { 1, 3, -2, 5, 10 };
+
+            int[] a4 = { 1, 3, 2, 4, 5 };
+
+            Array myArray = new int[5];
+
+            Array myArray2 = Array.CreateInstance(typeof(int), 4);
+            myArray2.SetValue(12, 0);
+
+            Console.WriteLine(myArray2.GetValue(0));
+        }
+    }
+}
