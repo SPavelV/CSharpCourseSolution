c107b0a297a6044207c1422cf30be7d384a5ba0c
Pavel Solovev <pashius2010@gmail.com>
2021-01-14 06:50:35 +0400

    first class

---
+++ D_OOP/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_OOP
+{
+    class Character
+    {
+        int Health = 100;
+
+        void Hit(int damage)
+        {
+            Health -= damage;
+        }
+    }
+}
