71d9df990024cbd39bc95594fe1d075a205400dc
Pavel Solovev <pashius2010@gmail.com>
2021-01-14 06:00:37 +0400

    multydim array

--- C_ArraysCollections/Program.cs
+++ C_ArraysCollections/Program.cs
@@ -8,7 +8,29 @@ namespace C_ArraysCollections
     {
         static void Main(string[] args)
         {
-            
+           
+        }
+
+        static void MultidimArray()
+        {
+            // 1 2 3 4
+            // 1 2 3
+
+            // 1 2 3
+            // 4 5 6
+
+            int[,] r1 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] r2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < r2.GetLength(0); i++)
+            {
+                for (int j = 0; j < r2.GetLength(1); j++)
+                {
+                    Console.Write($"{r2[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
 
         static void StackQueue()
