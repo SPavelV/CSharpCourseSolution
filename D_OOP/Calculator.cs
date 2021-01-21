64be2b5392bfbdf4bd9a3d6cd1ccc51e6a52117c
Pavel Solovev <pashius2010@gmail.com>
2021-01-21 13:21:37 +0400

    output out parameters

--- D_OOP/Calculator.cs
+++ D_OOP/Calculator.cs
@@ -4,9 +4,20 @@ using System.Text;
 
 namespace D_OOP
 {
-    public static class Calculator
+    public class Calculator
     {
-        public static double Avarage(int[] numbers)
+        public bool TryDivide(double divisible, double divisor, out double result)
+        {
+            result = 0;
+            if(divisor == 0)
+            {
+                return false;
+            }
+            result = divisible / divisor;
+            return true;
+        }
+
+        public double Avarage(int[] numbers)
         {
             double sum = 0;
 
@@ -18,7 +29,7 @@ namespace D_OOP
             return sum / numbers.Length;
         }
 
-        public static double Avarage2(params int[] numbers)
+        public double Avarage2(params int[] numbers)
         {
             double sum = 0;
 
@@ -30,18 +41,18 @@ namespace D_OOP
             return sum / numbers.Length;
         }
 
-        public static double CalcTriangleSquare(double ab, double bc, double ca)
+        public double CalcTriangleSquare(double ab, double bc, double ca)
         {
             double semyPerimeter = (ab + bc + ca) / 2;
             return Math.Sqrt(semyPerimeter * (semyPerimeter - ab) * (semyPerimeter - bc) * (semyPerimeter - ca));
         }
 
-        public static double CalcTriangleSquare(double b, double h)
+        public double CalcTriangleSquare(double b, double h)
         {
             return 0.5 * b * h;
         }
 
-        public static double CalcTriangleSquare(double a, double b, int alpha)
+        public double CalcTriangleSquare(double a, double b, int alpha)
         {
             double rads = alpha * Math.PI / 180;
             return 0.5 * a * b * Math.Sin(rads);
--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -6,8 +6,32 @@ namespace D_OOP
     {
         static void Main(string[] args)
         {
-            Calculator.CalcTriangleSquare(a: 10, b: 20, alpha: 30);
+            Calculator calc = new Calculator();
 
+            if(calc.TryDivide(10, 0, out double result))
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Failed to divide");
+            }
+            Console.ReadLine();
+
+
+            Console.WriteLine("Enter a number, please.");
+
+            string line = Console.ReadLine();
+
+            bool wasParsed = int.TryParse(line, out int number);
+            if (wasParsed)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Failed to parse");
+            }
         }
 
     }
