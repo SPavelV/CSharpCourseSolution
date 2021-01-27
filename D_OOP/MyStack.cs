8d152709dc1bd1ce0c7a798d2a7acb9904d94dd2
Pavel Solovev <pashius2010@gmail.com>
2021-01-28 00:22:27 +0400

    Lazy evaluation and yield

--- D_OOP/MyStack.cs
+++ D_OOP/MyStack.cs
@@ -63,15 +63,24 @@ namespace D_OOP
             return _items[Count - 1];
         }
 
+        //public IEnumerator<T> GetEnumerator()
+        //{
+        //    return new StackEnumerator<T>(_items, Count);
+        //}
+
         public IEnumerator<T> GetEnumerator()
         {
-            return new StackEnumerator<T>(_items, Count);
+            for(int i = Count - 1; i>=0; i--)
+            {
+                yield return _items[i];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
+
     }
 
     public class StackEnumerator<T> : IEnumerator<T>
