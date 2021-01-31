00b08df37e30952597a32bce6a757b27ecab2b54
Pavel Solovev <pashius2010@gmail.com>
2021-01-31 19:30:04 +0400

    exception handling

--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace D_OOP
 {
@@ -8,37 +9,7 @@ namespace D_OOP
 
         static void Main(string[] args)
         {
-            MyStack<int> ms = new MyStack<int>();
-
-            ms.Push(1);
-            ms.Push(2);
-            ms.Push(3);
-
-            foreach(var item in ms){
-                Console.WriteLine(item);
-            }
-
-            Console.ReadLine();
-
-            while(ms.Count != 0)
-            {
-                Console.WriteLine((int)ms.Peek());
-                ms.Pop();
-            }
-
-            Console.WriteLine(ms.Peek());
-
-            ms.Pop();
-
-            Console.WriteLine(ms.Peek());
-
-            ms.Push(3);
-            ms.Push(4);
-            ms.Push(5);
-
-            Console.WriteLine(ms.Peek());
-
-            Console.ReadLine();
+          
         }
 
         static void ProblemOfRepresentatives()
---
+++ E_Exceptions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace E_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = null;
+
+            try
+            {
+                file = File.Open("temp.txt", FileMode.Open);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                if (file != null)
+                {
+                    file.Dispose();
+                }
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("Please input a number");
+
+            string result = Console.ReadLine();
+
+            int number = 0;
+
+            try
+            {
+                number = int.Parse(result);
+            }
+            catch (OverflowException ex)
+            {
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("A format exception has occured.");
+                Console.WriteLine("Information is below:");
+                Console.WriteLine(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
