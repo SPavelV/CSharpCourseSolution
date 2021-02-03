d986f99bedd363842cf697152bcc30aadbf8f50c
Pavel Solovev <pashius2010@gmail.com>
2021-02-03 20:10:22 +0400

    delegation

---
+++ G_Delegation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace G_Delegation
+{
+    public class Car
+    {
+        int speed = 0;
+        public delegate void TooFast(int currentSpeed);
+
+        private TooFast tooFast;
+
+        public void Start()
+        {
+            speed = 10;
+        }
+
+        public void Accelerate()
+        {
+            speed += 10;
+
+            if(speed > 80)
+            {
+                tooFast(speed);
+            }
+        }
+
+        public void Stop()
+        {
+            speed = 0;
+        }
+
+        public void RegisterOnTooFast(TooFast tooFast)
+        {
+            this.tooFast = tooFast;
+        }
+    }
+    class Program
+    {
+        static Car car;
+        static void Main(string[] args)
+        {
+            car = new Car();
+            car.RegisterOnTooFast(HandleOnTooFast);
+
+            car.Start();
+
+            for(int i = 0; i < 10; i++)
+            {
+                car.Accelerate();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void HandleOnTooFast(int speed)
+        {
+            Console.WriteLine($"Oh, I got it, calling stop! Current speed={speed}");
+            car.Stop();
+        }
+    }
+}
