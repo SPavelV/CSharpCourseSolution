bfa32178b0e9d19d4cb4163899477333ea14d3e8
Pavel Solovev <pashius2010@gmail.com>
2021-02-08 12:03:34 +0400

    intro LINQ, lyambda

--- G_Delegation/Program.cs
+++ G_Delegation/Program.cs
@@ -1,8 +1,118 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Timers;
 
 namespace G_Delegation
 {
+    public static class LinqExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null)
+                throw new ArgumentNullException();
+
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+    class Program
+    {
+        static Car car;
+        static void Main(string[] args)
+        {
+            DisplayLargestFilesWithLinq(@"E:\games");
+            DisplayLargestFilesWithoutLinq(@"E:\games");
+            Console.ReadLine();
+        }
+
+        private static void DisplayLargestFilesWithLinq(string pathToDir)
+        {
+            new DirectoryInfo(pathToDir)
+                .GetFiles()
+                .OrderByDescending(file => file.Length)
+                .Take(5)
+                .ForEach(file => Console.WriteLine($"{file.Name} weights {file.Length}"));
+
+            //IEnumerable<FileInfo> orderedFiles = new DirectoryInfo(pathToDir)
+            //    .GetFiles()
+            //    .OrderBy(file => file.Length)
+            //    .Take(5);
+
+            //foreach(var file in orderedFiles)
+            //{
+            //    Console.WriteLine($"{file.Name} weights {file.Length}");
+            //}
+        }
+
+        //static long KeySelector(FileInfo file)
+        //{
+        //    return file.Length;
+        //}
+
+        private static void DisplayLargestFilesWithoutLinq(string pathToDir)
+        {
+            var dirInfo = new DirectoryInfo(pathToDir);
+            FileInfo[] files = dirInfo.GetFiles();
+
+            Array.Sort(files, FilesComparison);
+
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name} weights {file.Length}");
+            }
+        }
+
+        private static int FilesComparison(FileInfo x, FileInfo y)
+        {
+            if (x.Length == y.Length) return 0;
+            if (x.Length > y.Length) return -1;
+            return 1;
+        }
+
+        private static void EventDemo()
+        {
+            Timer timer = new Timer();
+            timer.Elapsed += Timer_Elapsed;
+
+            timer.Interval = 5000;
+            timer.Start();
+
+            Console.ReadLine();
+
+            car = new Car();
+            car.TooFastDriving += HandleOnTooFast;
+            car.TooFastDriving += HandleOnTooFast;
+
+            car.TooFastDriving -= HandleOnTooFast;
+
+            car.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                car.Accelerate();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //var timer = (Timer)sender;
+            Console.WriteLine("Handler Timer Elapsed Event");
+        }
+
+        private static void HandleOnTooFast(int speed)
+        {
+            Console.WriteLine($"Oh, I got it, calling stop! Current speed={speed}");
+            car.Stop();
+        }
+    }
+
     public class Car
     {
         int speed = 0;
@@ -23,9 +133,9 @@ namespace G_Delegation
         {
             speed += 10;
 
-            if(speed > 80)
+            if (speed > 80)
             {
-                if(TooFastDriving !=null)
+                if (TooFastDriving != null)
                     TooFastDriving(speed);
             }
         }
@@ -44,46 +154,5 @@ namespace G_Delegation
         //{
         //    this.tooFast -= tooFast;
         //}
-    }
-    class Program
-    {
-        static Car car;
-        static void Main(string[] args)
-        {
-            Timer timer = new Timer();
-            timer.Elapsed += Timer_Elapsed;
-
-            timer.Interval = 5000;
-            timer.Start();
-
-            Console.ReadLine();
-
-            car = new Car();
-            car.TooFastDriving += HandleOnTooFast;
-            car.TooFastDriving += HandleOnTooFast;
-
-            car.TooFastDriving -= HandleOnTooFast;
-
-            car.Start();
-
-            for(int i = 0; i < 10; i++)
-            {
-                car.Accelerate();
-            }
-
-            Console.ReadLine();
-        }
-
-        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            //var timer = (Timer)sender;
-            Console.WriteLine("Handler Timer Elapsed Event");
-        }
-
-        private static void HandleOnTooFast(int speed)
-        {
-            Console.WriteLine($"Oh, I got it, calling stop! Current speed={speed}");
-            car.Stop();
-        }
     }
 }
